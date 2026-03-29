19fdd8fb1ba01fa2a5815baded2fe766cb876a1c
TMWL214wizard <rsossaman@basecampcodingacademy.org>
2026-03-29 14:15:05 -0500

    urls and models(beginning)

--- Brad's Bees/app/models.py
+++ Brad's Bees/app/models.py
@@ -1,3 +1,60 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    variant = models.CharField(max_length=20, blank=True, null=True)
+    description = models.TextField()
+    price = models.DecimalField()
+    is_acive = models.BooleanField(default=True)
+
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=2,
+                             choices=STATE_CHOICES, # write or pip somehow
+                             default="")
+    zip_code = models.CharField(max_length=5)
+
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+
+
+class Order(models.Model):
+    DELIVERY = "delivery"
+    PICKUP = "pickup"
+
+    FULFILLMENT_CHOICES = [
+        (DELIVERY, "delivery"),
+        (PICKUP, "pickup"),
+    ]
+
+    #-- customer info --#
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    address = models.ForeignKey(Address, on_delete=models.CASCADE, related_name="order")
+
+    #-- order info --#
+    fulfillment_method = models.CharField(max_length=10,
+                                          choices=FULFILLMENT_CHOICES,
+                                          default="")
+    total_price = models.DecimalField()
+    created_at = models.DateTimeField()
+
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="order_items")
+
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField()
+
+
+
+# business settings and import geopy for them
--- Brad's Bees/config/urls.py
+++ Brad's Bees/config/urls.py
@@ -16,7 +16,26 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import app.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+#|=================|#
+#|--- USER SIDE ---|#
+#|=================|#
+
+    path('', app.views.index, name="index"),
+    path('home/', app.views.home, name="home"),
+    path('removal/', app.views.bee_removal, name="removal"),
+    path('shop/', app.views.shop, name="shop"),
+    path('education/', app.views.education, name="education"),
+    path('gallery/', app.views.gallery, name="gallery"),
+
+#|==================|#
+#|--- ADMIN SIDE ---|#
+#|==================|#
+
+    path('admin_home/', app.views.admin, name="admin")
+
 ]
