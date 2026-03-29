b0c24bca35879548e86b12a0c35ff99c309fee42
Tyler32J <tfarrell@basecampcodingacademy.org>
2026-03-29 01:46:27 -0500

    Added Html & CSS files

--- Brad's Bees/config/settings.py
+++ Brad's Bees/config/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from decouple import config, Csv
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -55,7 +58,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'app/template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
