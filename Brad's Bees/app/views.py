10e99c1f1eb9b1c8ff702525d044e25cdc85a3f6
TMWL214wizard <rsossaman@basecampcodingacademy.org>
2026-03-31 17:36:07 -0500

    added views to render pages. need notes for what exactly we want to do on each page.

--- Brad's Bees/app/views.py
+++ Brad's Bees/app/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
 
 # Create your views here.
+def admin_home(request):
+    return render("admin_home.html")
+
+
+
+def bee_removal(request):
+    return render("bee_removal.html")
+
+
+
+def render_ed_page(request):
+    return render("educational.html")
+
+
+
+def render_gallery(request):
+    return render("gallery.html")
+
+
+
+def render_home_page(request):
+    return render("home.html")
+
+
+
+def render_index(request):
+    return render("index.html")
+
+
+
+def shop(request):
+    return render("shop.html")
