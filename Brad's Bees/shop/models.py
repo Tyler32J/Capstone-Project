b070775c27eb1b415966dc3026a8159aa0ff2373
TMWL214wizard <rsossaman@basecampcodingacademy.org>
2026-04-04 21:04:09 -0500

    missing attributes from Product model caused error, fixed.

--- Brad's Bees/shop/models.py
+++ Brad's Bees/shop/models.py
@@ -7,8 +7,8 @@ class Product(models.Model):
     name = models.CharField(max_length=30)
     variant = models.CharField(max_length=20, null=True, blank=True)
     description = models.CharField(max_length=999)
-    price = models.DecimalField(max_digits=2)
-    stock = models.PositiveIntegerField(max_value=999)
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+    stock = models.PositiveIntegerField()
 
     def __str__(self):
         if self.variant:
@@ -18,7 +18,7 @@ class Product(models.Model):
 
 
 class Cart(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     
     def grand_total(self):
