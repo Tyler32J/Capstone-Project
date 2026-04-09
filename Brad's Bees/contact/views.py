57ccad45df8a31b3fabac2617277262587b57139
TMWL214wizard <rsossaman@basecampcodingacademy.org>
2026-04-09 18:11:50 -0500

    error messages added to contact page; email notification function added to view

--- Brad's Bees/contact/views.py
+++ Brad's Bees/contact/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail
+from django.conf import settings
+from django.contrib import messages
 from .forms import *
 from .models import *
 
@@ -17,13 +20,47 @@ def contact_view(request):
                     image = f
                 )
 
+            subject = "New Contact Form Submission"
+            message = f"""
+
+            You have received a new contact form submission.
+
+            Name: {submission.name}
+            Email: {submission.email}
+            Message: {submission.message}
+
+            """
+
+            send_mail(
+                subject,
+                message,
+                settings.DEFAULT_FROM_EMAIL,
+                [business@example.com], # change to Brad's email
+                fail_silently = False,
+            )
+
+            messages.success(request, "Form submitted successfully!")
+
             sub_count = Submission.objects.count()
             print(f"\nSUB COUNT: {sub_count}") # bash test for sub C func.
 
             image_count = SubmissionImage.objects.count()
             print(f"IMG COUNT: {image_count}\n") # bash test for img C func.
 
+            return redirect('contact')
+    
+            
     else:
         form = SubmissionForm()
 
     return render(request, 'contact.html', {'form': form})
+
+#### FOLLOWING IS EMAIL SETUP TEMPLATE FOR LAUNCH == SETTINGS.PY ####
+
+#  EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#  EMAIL_HOST = 'smtp.gmail.com'
+#  EMAIL_PORT = 587
+#  EMAIL_USE_TLS = True
+#  EMAIL_HOST_USER = 'your_email@gmail.com'
+#  EMAIL_HOST_PASSWORD = 'your_app_password'
+#  DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
