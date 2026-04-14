0da7b595c4ab4061e267a760ea5f604fe8a6b7b5
TMWL214wizard <rsossaman@basecampcodingacademy.org>
2026-04-14 18:07:54 -0500

    updated email settings and contact view

--- Brad's Bees/config/settings.py
+++ Brad's Bees/config/settings.py
@@ -126,4 +126,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'your_email@gmail.com'
+EMAIL_HOST_PASSWORD = 'your_app_password'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
--- Brad's Bees/contact/views.py
+++ Brad's Bees/contact/views.py
@@ -54,13 +54,3 @@ def contact_view(request):
         form = SubmissionForm()
 
     return render(request, 'contact.html', {'form': form})
-
-#### FOLLOWING IS EMAIL SETUP TEMPLATE FOR LAUNCH == SETTINGS.PY ####
-
-#  EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-#  EMAIL_HOST = 'smtp.gmail.com'
-#  EMAIL_PORT = 587
-#  EMAIL_USE_TLS = True
-#  EMAIL_HOST_USER = 'your_email@gmail.com'
-#  EMAIL_HOST_PASSWORD = 'your_app_password'
-#  DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
