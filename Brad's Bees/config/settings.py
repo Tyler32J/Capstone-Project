9a6ad542035073f106ede8cb1865e36bba700b32
Tyler32J <tfarrell@basecampcodingacademy.org>
2026-04-28 18:55:32 -0500

    "Update"

--- Brad's Bees/config/settings.py
+++ Brad's Bees/config/settings.py
@@ -139,7 +139,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # MOVE BELOW TO ENVIRONMENT VARIABLES
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
